4b12a5e1a3fdd95ff5ba7b9909c13b932b334c94
LadanRazeghi <ladan.razeghi@sky.uk>
2023-03-28 15:44:43 +0100

    Added Order Script

---
+++ EmpowerScript2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerScript2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerScript2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("ABD0B886-1B72-4D7D-A8F9-02E1B680DAF7")]
+[assembly: AssemblyVersion("1.0.0.0")]
